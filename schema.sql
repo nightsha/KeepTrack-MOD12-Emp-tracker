53d34cb28827498839b308fe052a9d0bc5b7105b
nightsha <130249622+nightsha@users.noreply.github.com>
2023-10-20 19:45:16 -0500

    db&tablesCreated

---
+++ schema.sql
@@ -0,0 +1,40 @@
+-- drops database if it exists
+DROP DATABASE IF EXISTS employee tracker_db;
+-- creates database if it exists
+CREATE DATABASE employee tracker_db;
+
+
+-- use employee tracker_db
+USE employee tracker_db;
+
+-- -- Creates the table "department" within department_db --
+CREATE TABLE department (
+
+    --  -- Creates a numeric column called "id" --
+    id INT NOT NULL
+    -- Makes a string column called "name" which cannot contain null
+    name VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE role (
+    id: INT PRIMARY KEY
+
+title: VARCHAR(30) to hold role title
+
+salary: DECIMAL to hold role salary
+
+department_id: INT
+
+)
+
+CREATE TABLE employee (
+    id: INT PRIMARY KEY
+
+first_name: VARCHAR(30) NOT NULL
+
+last_name: VARCHAR(30) NOT NULL
+
+role_id: INT to hold reference to employee role
+
+manager_id: INT 
+)
