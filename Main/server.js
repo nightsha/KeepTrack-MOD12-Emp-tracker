a0a73ef6fe7c6cc76e3e18c8b3c12a945d9b510f
nightsha <130249622+nightsha@users.noreply.github.com>
2023-11-12 19:17:01 -0600

    addedCRUDtoServer

--- Main/server.js
+++ Main/server.js
@@ -8,3 +8,81 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password here
+      password: '',
+      database: 'employee_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+  );
+
+  // Create a employee
+app.post('/api/new-employee', ({ body }, res) => {
+    const sql = `INSERT INTO employees (employee_name)
+      VALUES (?)`;
+    const params = [body.employee_name];
+    
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      });
+    });
+  });
+
+  // Read all employees
+app.get('/api/employees', (req, res) => {
+    const sql = `SELECT id, employee_name AS title FROM employees`;
+    
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+         return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+  // Delete a employee
+app.delete('/api/employee/:id', (req, res) => {
+    const sql = `DELETE FROM employees WHERE id = ?`;
+    const params = [req.params.id];
+    
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.statusMessage(400).json({ error: res.message });
+      } else if (!result.affectedRows) {
+        res.json({
+        message: 'Movie not found'
+        });
+      } else {
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
+      }
+    });
+  });
+
+  // Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
